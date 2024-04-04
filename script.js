7b0d98822bbf81ce7276eef330a948679ad433a2
Chalex <marmolchalex@gmail.com>
2024-04-04 10:45:26 +0800

    fix(index.html and folder styles): make the main page responsive (not 100% responsive yet)

---
+++ script.js
@@ -0,0 +1,13 @@
+// Hiding the header according to pixels scrolled
+window.onscroll = function () {
+	const header = document.getElementById('header');
+	const scrollPosition = window.scrollY; // Get scroll position
+	console.log(scrollPosition);
+	const hideAfter = 500; // Pixels to scroll before hiding
+
+	if (scrollPosition > hideAfter) {
+		header.classList.add('hidden');
+	} else {
+		header.classList.remove('hidden');
+	}
+};
